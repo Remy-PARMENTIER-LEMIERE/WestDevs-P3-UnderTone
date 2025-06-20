5199199bd5c70570048d9277328e23ce83f81458
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-06-20 14:42:10 +0200

    Created splash-screen, is animated, router is adapted. Need to create another intermediary file for path /app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 // Import the main app component
 import App from "./App";
+import SplashScreen from "./pages/SplashScreen/SplashScreen";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -22,8 +23,18 @@ const router = createBrowserRouter([
   {
     path: "/", // The root path
     element: <App />, // Renders the App component for the home page
+    children: [
+      {
+        path: "",
+        element: <SplashScreen />,
+      },
+      {
+        path: "app/",
+        // element: ,
+        // children: ,
+      },
+    ],
   },
-  // Try adding a new route! For example, "/about" with an About component
 ]);
 
 /* ************************************************************************* */
