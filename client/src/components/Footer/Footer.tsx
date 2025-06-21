844a98c6fc35b362cc880f01dcbfc9938191ba4b
Bamba_DIOP <khadinho@live.fr>
2025-06-21 02:23:56 +0200

    footer work in progress

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://twiter.com" target="_blank" rel="noreferrer">
+        <img src="images/logo_x.svg" alt="logo x" />
+      </a>
+      <ul>
+        <li>
+          <p>Conditions Générales d’utilisation</p>
+        </li>
+        <li>
+          <p>Équipe de développement</p>
+        </li>
+        <li>
+          <p>© Copyright UnderTone 2025</p>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+export default Footer;
