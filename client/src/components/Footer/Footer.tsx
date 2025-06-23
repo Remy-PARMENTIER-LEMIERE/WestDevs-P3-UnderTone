1b4dd0a4e3aaa75fdbc4a0da42bded3070c8a5a5
Bamba_DIOP <khadinho@live.fr>
2025-06-23 11:29:11 +0200

    Overflow hidden set from body to footer. Created new wrapper enlarged to only see inner shadow on top border.

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,20 +3,22 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer>
-      <a href="https://twiter.com" target="_blank" rel="noreferrer">
-        <img src="images/logo_x.svg" alt="logo x" />
-      </a>
-      <ul>
-        <li>
-          <p>Conditions Générales d’utilisation</p>
-        </li>
-        <li>
-          <p>Équipe de développement</p>
-        </li>
-        <li>
-          <p>© Copyright UnderTone 2025</p>
-        </li>
-      </ul>
+      <section>
+        <a href="https://twiter.com" target="_blank" rel="noreferrer">
+          <img src="images/logo_x.svg" alt="logo x" />
+        </a>
+        <ul>
+          <li>
+            <p>Conditions Générales d’utilisation</p>
+          </li>
+          <li>
+            <p>Équipe de développement</p>
+          </li>
+          <li>
+            <p>© Copyright UnderTone 2025</p>
+          </li>
+        </ul>
+      </section>
     </footer>
   );
 }
