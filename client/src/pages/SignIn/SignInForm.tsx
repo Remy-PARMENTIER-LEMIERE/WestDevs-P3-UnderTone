9f080b6bd4e75bf7ba97a200008dafea38131cc1
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-06-23 16:02:17 +0200

    feature/signInPage/US6 signInPage done

--- client/src/pages/SignIn/SignInForm.tsx
+++ client/src/pages/SignIn/SignInForm.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router";
 import "./SignInForm.css";
 
 function SignInForm() {
   return (
     <div className="signinForm-container">
       <section className="form-container">
-        <h2>Inscription</h2>
+        <h1>Inscription</h1>
 
         <div className="customRadioHolder">
           <input
@@ -45,39 +46,27 @@ function SignInForm() {
         </div>
 
         <form className="signinform">
-          <input
-            type="text"
-            name="Identifiant"
-            aria-label="identifiant"
-            placeholder="Identifiant"
-            id="identifiant"
-          />
-          <input
-            type="email"
-            name="email"
-            aria-label="email"
-            placeholder="Email"
-            id="email"
-          />
-          <input
-            type="text"
-            name="mot de passe"
-            aria-label="mot de passe"
-            placeholder="Mot de passe"
-            id="password"
-          />
-          <input
-            type="text"
-            name="vérification mot de passe"
-            aria-label="Vérification Mot de passe"
-            placeholder="Vérifier votre mot de passe"
-            id="password"
-          />
+          <div className="inputGroup">
+            <input type="text" required autoComplete="off" />
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className="inputGroup">
+            <input type="email" required autoComplete="on" />
+            <label htmlFor="Email">Email</label>
+          </div>
+          <div className="inputGroup">
+            <input type="password" required autoComplete="off" />
+            <label htmlFor="password">Mot de passe</label>
+          </div>
+          <div className="inputGroup">
+            <input type="password" required autoComplete="off" />
+            <label htmlFor="verified-password">Vérifier mot de passe</label>
+          </div>
         </form>
         <button type="button" className="signin-button">
-          s'inscrire
+          S'inscrire
         </button>
-        <a href="lklsf">Déjà un compte ? Connectez-vous</a>
+        <Link to="lklsf">Déjà un compte ? Connectez-vous</Link>
       </section>
     </div>
   );
