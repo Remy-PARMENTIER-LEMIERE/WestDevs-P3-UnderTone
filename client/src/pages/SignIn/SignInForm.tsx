9c2c859a788bf5bf52c74f78419e8966db9b6f0a
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-06-23 16:36:09 +0200

    changes done

--- client/src/pages/SignIn/SignInForm.tsx
+++ client/src/pages/SignIn/SignInForm.tsx
@@ -3,62 +3,62 @@ import "./SignInForm.css";
 
 function SignInForm() {
   return (
-    <div className="signinForm-container">
+    <main className="signin-page">
       <section className="form-container">
         <h1>Inscription</h1>
 
-        <div className="customRadioHolder">
+        <div className="custom-radio-holder">
           <input
-            className="customRadioInput"
+            className="custom-radio-input"
             id="artist"
             type="radio"
-            name="myRadioGroup"
+            name="my-radio-group"
           />
-          <label className="customRadioWrapper" htmlFor="artist">
-            <div className="customRadio">
+          <label className="custom-radio-wrapper" htmlFor="artist">
+            <div className="custom-radio">
               <div className="inner">Artiste</div>
             </div>
           </label>
 
           <input
-            className="customRadioInput"
+            className="custom-radio-input"
             id="user"
             type="radio"
-            name="myRadioGroup"
+            name="my-radio-group"
           />
-          <label className="customRadioWrapper" htmlFor="user">
-            <div className="customRadio">
+          <label className="custom-radio-wrapper" htmlFor="user">
+            <div className="custom-radio">
               <div className="inner">User</div>
             </div>
           </label>
 
           <input
-            className="customRadioInput"
-            id="lieux"
+            className="custom-radio-input"
+            id="place"
             type="radio"
-            name="myRadioGroup"
+            name="my-radio-group"
           />
-          <label className="customRadioWrapper" htmlFor="lieux">
-            <div className="customRadio">
+          <label className="custom-radio-wrapper" htmlFor="lieux">
+            <div className="custom-radio">
               <div className="inner">Lieux</div>
             </div>
           </label>
         </div>
 
-        <form className="signinform">
-          <div className="inputGroup">
+        <form className="signin-form">
+          <div className="input-group">
             <input type="text" required autoComplete="off" />
             <label htmlFor="name">Name</label>
           </div>
-          <div className="inputGroup">
+          <div className="input-group">
             <input type="email" required autoComplete="on" />
             <label htmlFor="Email">Email</label>
           </div>
-          <div className="inputGroup">
+          <div className="input-group">
             <input type="password" required autoComplete="off" />
             <label htmlFor="password">Mot de passe</label>
           </div>
-          <div className="inputGroup">
+          <div className="input-group">
             <input type="password" required autoComplete="off" />
             <label htmlFor="verified-password">Vérifier mot de passe</label>
           </div>
@@ -66,9 +66,9 @@ function SignInForm() {
         <button type="button" className="signin-button">
           S'inscrire
         </button>
-        <Link to="lklsf">Déjà un compte ? Connectez-vous</Link>
+        <Link to="/login-page">Déjà un compte ? Connectez-vous</Link>
       </section>
-    </div>
+    </main>
   );
 }
 
