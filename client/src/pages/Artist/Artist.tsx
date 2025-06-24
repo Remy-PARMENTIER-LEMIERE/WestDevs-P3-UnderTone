a993dfa07bd9f6b16e682e9b3ac65c395d90086f
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-06-24 15:28:50 +0200

    WIP

--- client/src/pages/Artist/Artist.tsx
+++ client/src/pages/Artist/Artist.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import Carousel from "../../components/Carousel/Carousel";
 import MusicStyles from "../../components/MusicStyles/MusicStyles";
 import SocialNetworks from "../../components/SocialNetworks/SocialNetworks";
 import "./Artist.css";
+import { useParams } from "react-router";
 
 function Artist() {
+  const params = useParams();
+  const [artist, setArtist] = useState();
+
+  useEffect(() => {
+    fetch(`http://localhost:3310/api/artist/${params}`)
+      .then((response) => response.json())
+      .then((data) => setArtist(data));
+  }, [params]);
   return (
     <main className="artist-page">
-      <h1>{}</h1>
+      <h1>{artist}</h1>
       <figure>
         <input
           type="checkbox"
