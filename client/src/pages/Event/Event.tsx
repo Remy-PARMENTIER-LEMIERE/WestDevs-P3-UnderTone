b3c13e57d451d31c05e468920bbc97511b99a8d4
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-07-01 13:54:02 +0200

    work in progress

---
+++ client/src/pages/Event/Event.tsx
@@ -0,0 +1,16 @@
+import "./Event.css";
+
+function Event() {
+  return (
+    <main>
+      <h1>Nom de la soirée</h1>
+      <img src="" alt="affiche de la soirée" />
+      <p>date</p>
+      <p>artiste invité</p>
+      <p>adresse</p>
+      <p>description</p>
+    </main>
+  );
+}
+
+export default Event;
