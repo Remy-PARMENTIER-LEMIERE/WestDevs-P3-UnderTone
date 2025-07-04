9f4ca1fabaa327a032e1f49fffb032b76e2adf0b
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-07-04 13:23:12 +0200

    Carousel  functionnal and styled. Might be adapted on styles.

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,42 +1,8 @@
 import type React from "react";
 import { useCallback, useRef, useState } from "react";
-// import "./TeamCarousel.css"; // Assurez-vous que le chemin est correct
 import "./Carousel.css";
 
-const teamMembers = [
-  {
-    name: "Emily Kim",
-    id: 1,
-    img: "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Michael Steward",
-    id: 2,
-    img: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Emma Rodriguez",
-    id: 3,
-    img: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmVzc2lvbmFsJTIwcGVvcGxlfHxlbnwwfHwwfHx8MA%3D%3D",
-  },
-  {
-    name: "Julia Gimmel",
-    id: 4,
-    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=900&auto=format&fit=q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmVzc2lvbmFsJTIwcGVvcGxlfHxlbnwwfHwwfHx8MA%3D%3D",
-  },
-  {
-    name: "Lisa Anderson",
-    id: 5,
-    img: "https://images.unsplash.com/photo-1655249481446-25d575f1c054?w=900&auto=format&fit=q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHByb2Zlc3Npb25hbCUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D",
-  },
-  {
-    name: "James Wilson",
-    id: 6,
-    img: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
-
-function Carousel() {
+function Carousel({ photosDatas }: { photosDatas: Photo[] }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -46,7 +12,7 @@ function Carousel() {
       setIsAnimating(true);
 
       const normalizedIndex =
-        (newIndex + teamMembers.length) % teamMembers.length;
+        (newIndex + photosDatas.length) % photosDatas.length;
       setCurrentIndex(normalizedIndex);
 
       setTimeout(() => {
@@ -54,7 +20,7 @@ function Carousel() {
         setIsAnimating(false);
       }, 800);
     },
-    [isAnimating],
+    [isAnimating, photosDatas],
   );
 
   const touchStartX = useRef(0);
@@ -78,13 +44,13 @@ function Carousel() {
 
   const getCardClass = (index: number) => {
     const offset =
-      (index - currentIndex + teamMembers.length) % teamMembers.length;
+      (index - currentIndex + photosDatas.length) % photosDatas.length;
     let cardClass = "";
     if (offset === 0) {
       cardClass = "center";
     } else if (offset === 1) {
       cardClass = "right-1";
-    } else if (offset === teamMembers.length - 1) {
+    } else if (offset === photosDatas.length - 1) {
       cardClass = "left-1";
     } else {
       cardClass = "hidden";
@@ -109,15 +75,15 @@ function Carousel() {
             ‹
           </button>
           <div className="carousel-track">
-            {teamMembers.map((member, index) => (
+            {photosDatas.map((photo, index) => (
               <button
                 type="button"
-                key={member.id}
+                key={photo.id}
                 className={`card ${getCardClass(index)}`}
                 data-index={index}
                 onClick={() => updateCarousel(index)}
               >
-                <img src={member.img} alt={`Team Member ${index + 1}`} />
+                <img src={photo.image} alt={`Postée le ${photo.date}`} />
               </button>
             ))}
           </div>
@@ -132,7 +98,7 @@ function Carousel() {
         </div>
 
         <div className="dots">
-          {teamMembers.map((member, index) => (
+          {photosDatas.map((member, index) => (
             <button
               type="button"
               key={member.id}
