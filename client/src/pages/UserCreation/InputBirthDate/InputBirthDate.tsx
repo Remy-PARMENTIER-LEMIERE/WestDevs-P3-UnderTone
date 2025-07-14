e6d042f68fd0e9ee480228ff82c3648a2fe8e29e
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-07-14 21:36:59 +0200

    user creation form functionnal but not styled

--- client/src/pages/UserCreation/InputBirthDate/InputBirthDate.tsx
+++ client/src/pages/UserCreation/InputBirthDate/InputBirthDate.tsx
@@ -1,7 +1,15 @@
 import "./InputBirthDate.css";
 
 function InputBirthDate() {
-  return <></>;
+  const today = new Date();
+  const todayString = today.toISOString().split("T")[0];
+
+  return (
+    <div className="input-group">
+      <input type="date" name="birthdate" id="birthdate" max={todayString} />
+      <label htmlFor="birthdate">Date de naissance</label>
+    </div>
+  );
 }
 
 export default InputBirthDate;
