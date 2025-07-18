cd05d153b99c52ad07f6e9f31c2f6aacdf9288e2
Eula-Coly <eulacoly@gmail.com>
2025-07-18 14:11:30 +0200

    contact_page done

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "../../assets/styles/forms.css";
 import "./Contact.css";
 
 function Contact() {
+  const [sent, setSent] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSent(true);
+  };
   return (
     <main className="contact-page">
       <h1>Contactez-nous</h1>
 
       <div className="contact-content">
         <section className="contact-form-container">
-          <form>
+          <form onSubmit={handleSubmit}>
             <h3>Formulaire de contact</h3>
 
             <div className="input-group">
@@ -53,18 +60,22 @@ function Contact() {
             </div>
 
             <button type="submit">Envoyer</button>
+            {sent && (
+              <p className="success-message">
+                Votre message a bien été envoyé !
+              </p>
+            )}
           </form>
         </section>
 
         <section className="contact-map-block">
           <iframe
-            className="google-map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43433.54264642648!2d-1.5866515!3d47.2186371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805eec447f61f25%3A0x40dc8d7053c41f0!2sNantes!5e0!3m2!1sfr!2sfr!4v1721300000000"
             title="Carte Nantes"
             allowFullScreen
             loading="lazy"
           />
-          <div className="contact-info">
+          <div>
             <p>
               <strong>Adresse :</strong>
               <br />
